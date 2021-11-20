0843d7fb7cd4b6ae30ef3c140c80453b546a6464
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-11-20 12:33:00 -0300

    Import icon, Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import github from './assets/images/github.png';
 import { Search } from './Components/Search/Search';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/romeiro-bru/typescript-ghibli-movies" target="_blank" rel="noopener noreferrer">
+        <img className="github" src={github} alt="icon" />
+      </a>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <Search />
+      <Footer />
     </div>
   );
 }
