53dac334da89d2eb6c9f37264621d434fe2883c7
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-11-23 16:40:05 -0300

    Add link to info page

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -7,7 +7,9 @@ export function Books({ books }) {
         return (
           <ul key={i}>
             <li>
-              <img src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+              <a href={book.volumeInfo.infoLink} target="_blank">
+                <img src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+              </a>
               <div>
                 <h2 className="book-title">{book.volumeInfo.title}</h2>
                 <p className="book-author">{book.volumeInfo.authors}</p>
