33fbde26c8e0f7170aa0c53ea1e2fedb103f57db
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-11-25 14:13:59 -0300

    Refactor, receive props

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,29 +1,60 @@
 import './style.css';
 
-export function Books({ books }) {
+const url = 'http://books.google.com/books/'
+
+export function Books({ books, category, hide }) {
   return (
-    <main className="books">
-      {books.map((book, i) => {
-        return (
-          <ul key={i}>
-            <li>
-              <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
-                <img src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
-              </a>
-              <div>
-                <h2 className="book-title">{book.volumeInfo.title}</h2>
-                <p className="book-author">{book.volumeInfo.authors}</p>
-                <p>{book.volumeInfo.pageCount} pages</p>
-                <p className="book-categorie">
-                  {book.volumeInfo.categories !== undefined ?
-                    book.volumeInfo.categories : 'Others'
-                  }
-                </p>
-              </div>
-            </li>
-          </ul>
-        )
-      })}
-    </main>
+    <>
+      <main className="books">
+        {books.map((book, i) => {
+          return (
+            <ul hidden={hide} key={i}>
+              <li>
+                <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
+                  <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+                </a>
+                <div>
+                  <h2 className="book-title">{book.volumeInfo.title}</h2>
+                  <p className="book-author">{book.volumeInfo.authors}</p>
+                  <p>{book.volumeInfo.pageCount} pages</p>
+                  <p className="book-categorie">
+                    {book.volumeInfo.categories !== undefined ?
+                      book.volumeInfo.categories : 'Others'
+                    }
+                  </p>
+                </div>
+              </li>
+            </ul>
+          )
+        })}
+
+        {category.map((book, i) => {
+          return (
+            <ul hidden={!hide} key={i}>
+              <li>
+                <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
+                  <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+                </a>
+                <div>
+                  <h2 className="book-title">{book.volumeInfo.title}</h2>
+                  <p className="book-author">{book.volumeInfo.authors}</p>
+                  <p>
+                    {book.volumeInfo.pageCount !== undefined ?
+                      book.volumeInfo.pageCount : '120'
+                    } pages
+                  </p>
+                  <p className="book-categorie">
+                    {book.volumeInfo.categories !== undefined ?
+                      book.volumeInfo.categories : 'Others'
+                    }
+                  </p>
+                </div>
+              </li>
+            </ul>
+          )
+        })}
+      </main>
+    </>
+
   )
 }
