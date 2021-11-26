bd2db55aa50f376d7596f6133359235eec6c68de
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-11-26 17:16:00 -0300

    Add verification to books render

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -6,53 +6,59 @@ export function Books({ books, category, hide }) {
   return (
     <>
       <main className="books">
-        {books.map((book, i) => {
-          return (
-            <ul hidden={hide} key={i}>
-              <li>
-                <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
-                  <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
-                </a>
-                <div>
-                  <h2 className="book-title">{book.volumeInfo.title}</h2>
-                  <p className="book-author">{book.volumeInfo.authors}</p>
-                  <p>{book.volumeInfo.pageCount} pages</p>
-                  <p className="book-categorie">
-                    {book.volumeInfo.categories !== undefined ?
-                      book.volumeInfo.categories : 'Others'
-                    }
-                  </p>
-                </div>
-              </li>
-            </ul>
-          )
-        })}
+        {
+          books === undefined ? [] :
+            books.map((book, i) => {
+              return (
+                <ul hidden={hide} key={i}>
+                  <li>
+                    <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
+                      <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+                    </a>
+                    <div>
+                      <h2 className="book-title">{book.volumeInfo.title}</h2>
+                      <p className="book-author">{book.volumeInfo.authors}</p>
+                      <p>{book.volumeInfo.pageCount} pages</p>
+                      <p className="book-categorie">
+                        {book.volumeInfo.categories !== undefined ?
+                          book.volumeInfo.categories : 'Others'
+                        }
+                      </p>
+                    </div>
+                  </li>
+                </ul>
+              )
+            })
+        }
 
-        {category.map((book, i) => {
-          return (
-            <ul hidden={!hide} key={i}>
-              <li>
-                <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
-                  <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
-                </a>
-                <div>
-                  <h2 className="book-title">{book.volumeInfo.title}</h2>
-                  <p className="book-author">{book.volumeInfo.authors}</p>
-                  <p>
-                    {book.volumeInfo.pageCount !== undefined ?
-                      book.volumeInfo.pageCount : '120'
-                    } pages
+        {
+          category === undefined ? [] :
+            category.map((book, i) => {
+              return (
+                <ul hidden={!hide} key={i}>
+                  <li>
+                    <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
+                      <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+                    </a>
+                    <div>
+                      <h2 className="book-title">{book.volumeInfo.title}</h2>
+                      <p className="book-author">{book.volumeInfo.authors}</p>
+                      <p>
+                        {book.volumeInfo.pageCount !== undefined ?
+                          book.volumeInfo.pageCount : '120'
+                        } pages
                   </p>
-                  <p className="book-categorie">
-                    {book.volumeInfo.categories !== undefined ?
-                      book.volumeInfo.categories : 'Others'
-                    }
-                  </p>
-                </div>
-              </li>
-            </ul>
-          )
-        })}
+                      <p className="book-categorie">
+                        {book.volumeInfo.categories !== undefined ?
+                          book.volumeInfo.categories : 'Others'
+                        }
+                      </p>
+                    </div>
+                  </li>
+                </ul>
+              )
+            })
+        }
       </main>
     </>
 
