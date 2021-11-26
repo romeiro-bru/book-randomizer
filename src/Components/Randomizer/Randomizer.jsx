eaeed547b2b1c285f7b8e7fd0c03c51019e9141f
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-11-26 15:29:48 -0300

    Send img props

--- src/Components/Randomizer/Randomizer.jsx
+++ src/Components/Randomizer/Randomizer.jsx
@@ -6,9 +6,8 @@ export function Randomizer({ category }) {
   const [randomized, setRandomized] = useState('')
 
   const randomize = () => {
-    return category.length !== 0 ?
-      category[Math.floor(Math.random() * category.length)].volumeInfo.title
-      : 'Select a category or Make your list'
+    return category.length !== 0 &&
+      category[Math.floor(Math.random() * category.length)].volumeInfo
   }
 
   const handleClick = () => {
@@ -17,8 +16,10 @@ export function Randomizer({ category }) {
 
   return (
     <div className="randomize tooltip">
-      <Modal handleClick={handleClick} randomized={randomized} />
-      <span className="tooltiptext">Pick a genre or make a list</span>
+      <Modal handleClick={handleClick}
+        bookImg={randomized.imageLinks !== undefined ? randomized.imageLinks.thumbnail : ""}
+        bookTitle={randomized.title} />
+      <p className="tooltiptext">Select a category or list and click here to generate your next reading!</p>
     </div>
   )
 }
