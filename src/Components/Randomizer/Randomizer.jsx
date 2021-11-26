acaba7256c24758597dfc54a20904230b67de1ec
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-11-25 21:53:00 -0300

    Import modal

--- src/Components/Randomizer/Randomizer.jsx
+++ src/Components/Randomizer/Randomizer.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
+import { Modal } from '../Modal/Modal';
 import './style.css';
 
-export function Randomizer() {
+export function Randomizer({ category }) {
+  const [randomized, setRandomized] = useState('')
+
+  const randomize = () => {
+    return category.length !== 0 ?
+      category[Math.floor(Math.random() * category.length)].volumeInfo.title
+      : 'Select a category'
+  }
+
   const handleClick = () => {
-    console.log('clicked')
+    setRandomized(randomize)
   }
 
   return (
     <div className="randomize tooltip">
       <button onClick={handleClick}>Randomize</button>
-      <span class="tooltiptext">Pick a genre or make a list</span>
+      <span className="tooltiptext">Pick a genre or make a list</span>
+
+      <p>{randomized}</p>
+      <Modal />
     </div>
   )
 }
