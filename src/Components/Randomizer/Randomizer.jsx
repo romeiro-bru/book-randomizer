d40e49d8601b3acd56bf8991a20790ba7ff6ca0c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-11-29 16:04:46 -0300

    Add dice img to tooltip

--- src/Components/Randomizer/Randomizer.jsx
+++ src/Components/Randomizer/Randomizer.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Modal } from '../Modal/Modal';
 import './style.css';
+import dices from '../../assets/images/dices.svg';
+
 
 export function Randomizer({ category }) {
   const [randomized, setRandomized] = useState('')
@@ -19,7 +21,10 @@ export function Randomizer({ category }) {
       <Modal handleClick={handleClick}
         bookImg={randomized.imageLinks !== undefined ? randomized.imageLinks.thumbnail : ""}
         bookTitle={randomized.title !== undefined ? randomized.title : 'Select a category or Make your list.'} />
-      <p className="tooltiptext">Select a category or list and click here to generate your next reading!</p>
+      <p className="tooltiptext">
+        Generate your next reading!
+        <img src={dices} alt="dices" />
+      </p>
     </div>
   )
 }
