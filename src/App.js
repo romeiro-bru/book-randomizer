43b81685b05cca5893fac1e64f84a046e8c0e167
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-12-08 16:22:53 -0300

    Import renamed component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Categories } from './Components/Categories/Categories';
-import { Books } from './Components/Books/Books';
+import { CategoryBooks } from './Components/CategoryBooks/CategoryBooks';
 import { Randomizer } from './Components/Randomizer/Randomizer';
 import { BookList } from './Components/BookList/BookList';
 import github from './assets/images/github.png';
@@ -23,7 +23,6 @@ function App() {
   const [search, setSearch] = useState('')
   const [books, setBooks] = useState([])
   const [category, setCategory] = useState([])
-  const [hide, setHide] = useState(true)
   const [list, setList] = useState([])
 
   useEffect(() => {
@@ -40,27 +39,19 @@ function App() {
 
   const handleChange = (e) => {
     setSearch(e.target.value)
-    setHide(false)
   }
-
+  console.log(category)
   const handleClick = (e) => {
-    setHide(true)
     const fetchCategory = async () => {
-      const response = await axios.get(`${url}/?q=subject:${e.target.value}`)
-      setCategory(response.data.items)
-    }
-    fetchCategory()
-  }
+      try {
+        const response = await axios.get(`${url}/?q=subject:${e.target.value}`)
+        setCategory(response.data.items)
+      } catch (error) {
+        console.log(error)
 
-  const handleAddToList = (book) => {
-    return list.length >= 8 ? list :
-      setList([...list, book])
-  }
-  const button = () => {
-    return (
-      <button className="add">
-      </button>
-    )
+      }
+    }
+    fetchCategory();
   }
 
   return (
@@ -68,17 +59,12 @@ function App() {
       <form className="search-box">
         <input onChange={handleChange} type="text" placeholder="Search" />
       </form>
-
       <aside>
         <Categories handleClick={handleClick} />
         <Randomizer category={category} list={list} />
-        {list.length > 0 ? <BookList list={list} /> :
-          ""
-        }
+        {list.length > 0 ? <BookList list={list} /> : ""}
       </aside>
-      <Books button={button} handleAddToList={handleAddToList}
-        hide={hide} books={books} category={category}
-      />
+      <CategoryBooks list={list} setList={setList} books={books} category={category} />
       <Footer />
     </div>
   );
