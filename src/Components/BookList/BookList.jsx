9c8f936e7fd9a0b5618b8561457f78033d43bf68
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-12-10 13:44:49 -0300

    Change ul

--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -1,18 +1,19 @@
 import './style.css';
+import imgnotavailable from '../../assets/images/imgnotavailable.png';
 
 export function BookList({ list }) {
   return (
     <section className="books-list">
-      {list.map((item, i) => (
-        <ul key={i}>
-          <li>
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>
             <img src={item.volumeInfo.imageLinks.smallThumbnail !== undefined ?
-              item.volumeInfo.imageLinks.smallThumbnail : ''
+              item.volumeInfo.imageLinks.smallThumbnail : { imgnotavailable }
             } alt="" />
             {item.volumeInfo.title}
           </li>
-        </ul>
-      ))}
+        ))}
+      </ul>
     </section>
   )
 }
