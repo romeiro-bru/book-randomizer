8f01b504258ca867fd4d8614dcd56316d380393c
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-12-15 18:23:20 -0300

    Create function to remove book from list

--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -1,14 +1,19 @@
 import './style.css';
 const url = 'http://books.google.com/books/'
 
-export function BookList({ list }) {
+export function BookList({ list, setList }) {
+  const handleRemoveBook = (book) => {
+    setList(list.filter((el) => el !== book))
+  }
+
   return (
     <section className="books-list">
       <ul>
-        {list.map((item, i) => (
+        {list.map((book, i) => (
           <li key={i}>
-            <img src={`${url}content?id=${item.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
-            {item.volumeInfo.title}
+            <button onClick={() => handleRemoveBook(book)}>x</button>
+            <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+            {book.volumeInfo.title}
           </li>
         ))}
       </ul>
