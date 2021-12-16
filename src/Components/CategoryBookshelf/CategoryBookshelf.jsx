9c64fd6d4b0f9afdcdc654483773e9a32330a7ef
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-12-16 19:03:15 -0300

    Create component, display items from created list

---
+++ src/Components/CategoryBookshelf/CategoryBookshelf.jsx
@@ -0,0 +1,49 @@
+import './style.css';
+import remove from '../../assets/images/x.png';
+
+const url = 'http://books.google.com/books/'
+
+export function CategoryBookshelf({ category, list, setList }) {
+  const handleRemoveBook = (book) => {
+    setList(list.filter((el) => el !== book))
+  }
+
+  console.log(category)
+  console.log(list)
+  return (
+    <>
+      <main className="books category-bookshelf">
+        <ul>
+          {list === undefined ? [] :
+            list.map((book, i) => {
+              return (
+                <li key={i}>
+                  <a href={book.volumeInfo.infoLink} target="_blank" rel="noreferrer">
+                    <img src={`${url}content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} alt="cover" />
+                  </a>
+                  <div>
+                    <h2 className="book-title">{book.volumeInfo.title}</h2>
+                    <p className="book-author">{book.volumeInfo.authors}</p>
+                    <p>
+                      {book.volumeInfo.pageCount !== undefined ?
+                        book.volumeInfo.pageCount : '120'
+                      } pages
+              </p>
+                    <p className="book-categorie">
+                      {book.volumeInfo.categories !== undefined ?
+                        book.volumeInfo.categories : 'Others'
+                      }
+                    </p>
+                  </div>
+                  <button onClick={() => handleRemoveBook(book)}>
+                    <img src={remove} alt="x" />
+                  </button>
+                </li>
+              )
+            }
+            )}
+        </ul>
+      </main>
+    </>
+  )
+}
